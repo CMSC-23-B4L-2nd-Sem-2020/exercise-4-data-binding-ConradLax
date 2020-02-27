c77b18ba8b00880b968002795175af4fe9cfec87
ConradLax <jmlaxamana@up.edu.ph>
2020-02-28 00:20:34 +0800

    feat: bind data to variable stringData

--- app/src/main/java/com/example/laxamana_exer4_slambook/MainActivity.kt
+++ app/src/main/java/com/example/laxamana_exer4_slambook/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     private val myName = StringData("Welcome!","Name","Nickname","Age","When is your birthday?","Phone number"
     ,"Email address","Course","What's your ultimate dream?","Who is your secret crush?","What is your message for me?",
         "Enter name","Enter nickname","Enter age","Month-Day-Year","09XX-XXX-XXXX","Enter email address"
-    ,"Enter course","Enter dream","Enter name of crush","Enter message","DONE")
+    ,"Enter course","Enter dream","Enter name of crush","Enter message","DONE","\n\nThank you for answering!")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,11 +28,50 @@ class MainActivity : AppCompatActivity() {
     private fun displayData() {
         binding.apply{
             stringData?.nameVal = nameEdit.text.toString()
+            stringData?.nicknameVal = nicknameEdit.text.toString()
+            stringData?.ageVal = ageEdit.text.toString()
+            stringData?.bdayVal = bdayEdit.text.toString()
+            stringData?.phoneVal = phoneEdit.text.toString()
+            stringData?.emailVal = emailEdit.text.toString()
+            stringData?.courseVal = courseEdit.text.toString()
+            stringData?.dreamVal = dreamEdit.text.toString()
+            stringData?.crushVal = crushEdit.text.toString()
+            stringData?.msgVal = msgEdit.text.toString()
             invalidateAll()
             greetingsText.visibility = View.GONE
             nameText.visibility = View.GONE
             nameEdit.visibility = View.GONE
+            nicknameText.visibility = View.GONE
+            nicknameEdit.visibility = View.GONE
+            ageText.visibility = View.GONE
+            ageEdit.visibility = View.GONE
+            bdayText.visibility = View.GONE
+            bdayEdit.visibility = View.GONE
+            phoneText.visibility = View.GONE
+            phoneEdit.visibility = View.GONE
+            emailText.visibility = View.GONE
+            emailEdit.visibility = View.GONE
+            courseText.visibility = View.GONE
+            courseEdit.visibility = View.GONE
+            dreamText.visibility = View.GONE
+            dreamEdit.visibility = View.GONE
+            crushText.visibility = View.GONE
+            crushEdit.visibility = View.GONE
+            msgText.visibility = View.GONE
+            msgEdit.visibility = View.GONE
+            submitButton.visibility = View.GONE
 
+            outputName.visibility = View.VISIBLE
+            outputNickname.visibility = View.VISIBLE
+            outputAge.visibility = View.VISIBLE
+            outputBday.visibility = View.VISIBLE
+            outputPhone.visibility = View.VISIBLE
+            outputEmail.visibility = View.VISIBLE
+            outputCourse.visibility = View.VISIBLE
+            outputDream.visibility = View.VISIBLE
+            outputCrush.visibility = View.VISIBLE
+            outputMsg.visibility = View.VISIBLE
+            responseText.visibility = View.VISIBLE
         }
 
 
--- app/src/main/java/com/example/laxamana_exer4_slambook/name_data.kt
+++ app/src/main/java/com/example/laxamana_exer4_slambook/name_data.kt
@@ -4,7 +4,7 @@ data class StringData(var greeting:String = "", var name: String = "", var nickn
                   var email: String = "", var course: String = "", var dream: String = "", var crush: String = "", var msg: String = "",
                   var nameHint: String = "", var nicknameHint: String = "", var ageHint: String = "", var bdayHint: String = "", var phoneHint: String = "",
                   var emailHint: String = "", var courseHint: String = "", var dreamHint: String = "", var crushHint: String = "", var msgHint: String = "",
-                      var buttonText:String = "",var nameVal:String = "", var nicknameVal:String = "", var ageVal:String = "", var bdayVal:String = "", var phoneVal:String = "",
+                      var buttonText:String = "",var responseText:String = "" , var nameVal:String = "", var nicknameVal:String = "", var ageVal:String = "", var bdayVal:String = "", var phoneVal:String = "",
                       var emailVal:String = "", var courseVal:String = "", var dreamVal:String = "", var crushVal:String = "", var msgVal:String = ""
 
                       )
